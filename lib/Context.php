2650ff05a5cf2fb9376b7207cf4bfd53b5424372
Bob Weinand <bobwei9@hotmail.com>
2022-05-21 22:34:18 +0200

    Add certain compiler-predefined defines float.h expects to exist

--- lib/Context.php
+++ lib/Context.php
@@ -22,6 +22,49 @@ class Context {
         '/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks/Kernel.framework/Versions/A/Headers/',
     ];
 
+    // defines needed for <float.h>
+    const NUMERICAL_DEFINES = [
+        "__DBL_DECIMAL_DIG__" => "17",
+        "__DBL_DENORM_MIN__" => "4.9406564584124654e-324",
+        "__DBL_DIG__" => "15",
+        "__DBL_EPSILON__" => "2.2204460492503131e-16",
+        "__DBL_HAS_DENORM__" => "1",
+        "__DBL_MANT_DIG__" => "53",
+        "__DBL_MAX_10_EXP__" => "308",
+        "__DBL_MAX_EXP__" => "1024",
+        "__DBL_MAX__" => "1.7976931348623157e+308",
+        "__DBL_MIN_10_EXP__" => "(-307)",
+        "__DBL_MIN_EXP__" => "(-1021)",
+        "__DBL_MIN__" => "2.2250738585072014e-308",
+        "__DECIMAL_DIG__" => "21",
+        "__FLT_DECIMAL_DIG__" => "9",
+        "__FLT_DENORM_MIN__" => "1.40129846e-45F",
+        "__FLT_DIG__" => "6",
+        "__FLT_EPSILON__" => "1.19209290e-7F",
+        "__FLT_EVAL_METHOD__" => "0",
+        "__FLT_HAS_DENORM__" => "1",
+        "__FLT_MANT_DIG__" => "24",
+        "__FLT_MAX_10_EXP__" => "38",
+        "__FLT_MAX_EXP__" => "128",
+        "__FLT_MAX__" => "3.40282347e+38F",
+        "__FLT_MIN_10_EXP__" => "(-37)",
+        "__FLT_MIN_EXP__" => "(-125)",
+        "__FLT_MIN__" => "1.17549435e-38F",
+        "__FLT_RADIX__" => "2",
+        "__LDBL_DECIMAL_DIG__" => "21",
+        "__LDBL_DENORM_MIN__" => "3.64519953188247460253e-4951L",
+        "__LDBL_DIG__" => "18",
+        "__LDBL_EPSILON__" => "1.08420217248550443401e-19L",
+        "__LDBL_HAS_DENORM__" => "1",
+        "__LDBL_MANT_DIG__" => "64",
+        "__LDBL_MAX_10_EXP__" => "4932",
+        "__LDBL_MAX_EXP__" => "16384",
+        "__LDBL_MAX__" => "1.18973149535723176502e+4932L",
+        "__LDBL_MIN_10_EXP__" => "(-4931)",
+        "__LDBL_MIN_EXP__" => "(-16381)",
+        "__LDBL_MIN__" => "3.36210314311209350626e-4932L",
+    ];
+
     public function __construct(array $headerSearchPaths = []) {
         if (PHP_INT_MAX > 1<<32) {
             $this->define('__x86_64__', new Token(Token::NUMBER, '1', 'built-in'));
@@ -32,6 +75,9 @@ class Context {
         $this->define('__GNUC__', new Token(Token::NUMBER, '4', 'built-in'));
         $this->define('__GNUC_MINOR__', new Token(Token::NUMBER, '2', 'built-in'));
         $this->define('__STDC__', new Token(Token::NUMBER, '1', 'built-in'));
+        foreach (self::NUMERICAL_DEFINES as $name => $value) {
+            $this->define($name, new Token(Token::NUMBER, $value, 'built-in'));
+        }
         $this->headerSearchPaths = array_merge($headerSearchPaths, self::DEFAULT_HEADER_SEARCH_PATHS);
         $this->locateGCCHeaderPaths();
         $this->scope = new Scope;
